356399fe41425e76cad5173a57f8117696193104
Hafiz <hafiz.dev24@gmail.com>
2025-12-13 23:06:10 +0600

    made select options and search bar responsive

--- src/pages/AllModels.jsx
+++ src/pages/AllModels.jsx
@@ -47,14 +47,14 @@ const AllModels = () => {
 
   return (
     <div className="w-11/12 mx-auto my-[30px]">
-      <div className="flex justify-between items-center mb-[30px]">
-        <div className="flex items-center gap-5">
+      <div className="flex justify-between items-center mb-20 md:mb-[30px]">
+        <div className="flex flex-col md:flex-row items-center gap-5">
           <h1 className="text-3xl font-semibold">Explore AI Models</h1>
           {/* select framework bar */}
           <select
             value={framework}
             onChange={handleSelectFramework}
-            className="border border-gray-300 rounded-sm px-2 py-1"
+            className="absolute left-5 top-37 md:static border border-gray-300 rounded-sm px-2 py-1"
             name=""
             id=""
           >
@@ -68,20 +68,22 @@ const AllModels = () => {
         </div>
 
         {/* search bar */}
-        <div className="relative">
-          <input
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            className="border border-gray-400 rounded-sm pl-7 py-1"
-            type="search"
-            name=""
-            id=""
-            placeholder="Search Models"
-          />
-          <FaSearch
-            className="absolute left-1.5 top-1/2 -translate-y-1/2 text-gray-400"
-            size={15}
-          />
+        <div className="absolute top-37 right-5 md:static">
+          <div className="relative">
+            <input
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className="border border-gray-400 rounded-sm pl-7 py-1 w-[162.4px] md:w-[170px] lg:w-full"
+              type="search"
+              name=""
+              id=""
+              placeholder="Search Models"
+            />
+            <FaSearch
+              className="absolute left-1.5 top-1/2 -translate-y-1/2 text-gray-400"
+              size={15}
+            />
+          </div>
         </div>
       </div>
 
